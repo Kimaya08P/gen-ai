2ac4e575f02b48cfbb40226feb721fad2dbb6456
Pushkar Asodekar <pushkar9aso@gmail.com>
2025-01-27 16:21:30 +0530

    Updated 3D layout and added 4D

--- data_vis/src/index.js
+++ data_vis/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FourDGraph from './4D/4Dgraph';
+import Three1 from './3D/Three1';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FourDGraph />
   </React.StrictMode>
 );
 
